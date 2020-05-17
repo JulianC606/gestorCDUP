d878d8afa2000de4c7218d311cad5cc661cb2921
Julián Cipagauta <julian.cipa96@gmail.com>
2020-05-17 02:45:21 -0500

    Se agrego handlebars

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import path from 'path'
+import sassMiddleware from 'node-sass-middleware'
+import hbs from 'express-handlebars'
 
 const app = express()
 
@@ -13,6 +16,37 @@ app.use(
   )
 )
 
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '../src/views'))
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultView: 'default',
+  defaultLayout: 'layout',
+  layoutsDir: path.join(__dirname, '../src/views/layouts/'),
+  partialsDir: path.join(__dirname, '../src/views/partials/')
+}))
+
+app.use(sassMiddleware({
+  /* Options */
+  src: path.join(__dirname, '../src/sass'),
+  dest: path.join(__dirname, '../public'),
+  debug: true,
+  outputStyle: 'compressed'
+}))
+
+app.use(
+  express.static(
+    path.join(
+      __dirname, 'public'
+    )
+  )
+)
+
+app.route('/')
+  .get((req, res, next) => {
+    res.render('pages/index')
+  })
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
