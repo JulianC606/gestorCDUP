48d53faa30d548d207a640463958c4ed04779e61
Julián Cipagauta <julian.cipa96@gmail.com>
2020-05-17 02:03:38 -0500

    finishing env

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+app.use(
+  bodyParser.json()
+)
+
+app.use(
+  bodyParser.urlencoded(
+    { extended: false }
+  )
+)
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
